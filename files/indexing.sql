896f35973dd41c69b433a6a64ccfee900f91c252
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-03-20 09:21:19 +0200

    new queries for city idx

--- files/indexing.sql
+++ files/indexing.sql
@@ -6,31 +6,28 @@ INSERT INTO unindexed_teams SELECT * FROM teams;
 SELECT COUNT(1) FROM unindexed_teams;
 SELECT COUNT(1) FROM indexed_teams;
 SELECT COUNT(1) FROM indexed_teams2;
+SELECT COUNT(1) FROM attendees1;
+SELECT COUNT(1) FROM attendees2;
 
--- INDEX ON LEAGUE_ID
-CREATE TABLE indexed_teams LIKE teams;
-INSERT INTO indexed_teams SELECT * FROM unindexed_teams;
-CREATE INDEX idx_league_id ON indexed_teams (league_id);
+CREATE TABLE attendees1 (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       match_id INT,
+       team_id INT,
+       is_home BOOLEAN NOT NULL,
+       is_winner BOOLEAN,
+       FOREIGN KEY (match_id) REFERENCES matches(id),
+       FOREIGN KEY (team_id) REFERENCES unindexed_teams(id)
+);
 
--- Unindexed (league_id)
-SELECT t.name, l.name
-FROM unindexed_teams t
-     JOIN leagues l on l.id = t.league_id
-WHERE league_id = 3
-ORDER BY t.name;
-
-# -> Sort: t.`name`  (cost=1.37e+6 rows=10.2e+6) (actual time=83949..84779 rows=7e+6 loops=1)
-#     -> Index lookup on t using league_id (league_id=3)  (cost=1.37e+6 rows=10.2e+6) (actual time=0.193..71096 rows=7e+6 loops=1)
-
--- Indexed (league_id)
-SELECT t.name, l.name
-FROM indexed_teams t
-    JOIN leagues l on l.id = t.league_id
-WHERE league_id = 3
-ORDER BY t.name;
-
-# -> Sort: t.`name`  (cost=1.39e+6 rows=10.4e+6) (actual time=30791..31334 rows=7e+6 loops=1)
-#     -> Index lookup on t using idx_league_id (league_id=3)  (cost=1.39e+6 rows=10.4e+6) (actual time=2.53..22409 rows=7e+6 loops=1)
+CREATE TABLE attendees2 (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       match_id INT,
+       team_id INT,
+       is_home BOOLEAN NOT NULL,
+       is_winner BOOLEAN,
+       FOREIGN KEY (match_id) REFERENCES matches(id),
+       FOREIGN KEY (team_id) REFERENCES indexed_teams2(id)
+);
 
 # INDEX ON CITY
 CREATE TABLE indexed_teams2 LIKE teams;
@@ -51,4 +48,42 @@ FROM indexed_teams2 t
 WHERE t.city = 'Madrid';
 
 # -> Filter: (t.city = 'Madrid')  (cost=2.2 rows=2) (actual time=5.48..5.51 rows=2 loops=1)
-#     -> Index lookup on t using idx_teams_city (city='Madrid')  (cost=2.2 rows=2) (actual time=5.13..5.16 rows=2 loops=1)
+#     -> Index lookup on t using idx_teams_city (city='Madrid')  (cost=2.2 rows=2) (actual time=5.13..5.16 rows=2 loops=1)    
+    
+    
+-- Unindexed (city) query 2
+SELECT t.id, t.city
+FROM football_system.attendees1 a
+    JOIN unindexed_teams t on t.id = a.team_id
+WHERE is_winner AND t.city = 'Ma%';
+
+# -> Nested loop inner join  (cost=1.17e+6 rows=99826) (actual time=9127..9127 rows=0 loops=1)
+#     -> Filter: (a.team_id is not null)  (cost=101845 rows=998256) (actual time=1.8..260 rows=1e+6 loops=1)
+#         -> Covering index scan on a using team_id  (cost=101845 rows=998256) (actual time=1.74..206 rows=1e+6 loops=1)
+#     -> Filter: (t.city = 'Ma%')  (cost=0.974 rows=0.1) (actual time=0.00879..0.00879 rows=0 loops=1e+6)
+#         -> Single-row index lookup on t using PRIMARY (id=a.team_id)  (cost=0.974 rows=1) (actual time=0.0086..0.00862 rows=1 loops=1e+6)
+
+-- Indexed (city) query 2
+SELECT t.id, t.city
+FROM attendees2 a
+     JOIN indexed_teams2 t on t.id = a.team_id
+WHERE is_winner AND t.city = 'Ma%';
+
+# -> Nested loop inner join  (cost=2.21 rows=1.09) (actual time=0.154..0.154 rows=0 loops=1)
+#     -> Filter: (t.city = 'Ma%')  (cost=1.1 rows=1) (actual time=0.14..0.14 rows=0 loops=1)
+#         -> Index lookup on t using idx_teams_city (city='Ma%')  (cost=1.1 rows=1) (actual time=0.14..0.14 rows=0 loops=1)
+#     -> Covering index lookup on a using team_id (team_id=t.id)  (cost=1.11 rows=1.09) (never executed)
+
+-- Unindexed (city) query 3
+EXPLAIN ANALYZE
+SELECT t.id, t.city
+FROM football_system.attendees1 a
+     JOIN unindexed_teams t on t.id = a.team_id
+WHERE is_winner AND t.city = '%or%';
+
+-- Indexed (city) query 3
+EXPLAIN ANALYZE
+SELECT t.id, t.city
+FROM attendees2 a
+     JOIN indexed_teams2 t on t.id = a.team_id
+WHERE is_winner AND t.city = '%or%';
