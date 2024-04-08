b8219c9f72a8d10e92a4aae129c4fbf101f955a1
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-04-08 18:57:36 +0300

    players goals stats

--- pa5/views.sql
+++ pa5/views.sql
@@ -1,19 +1,20 @@
 use football_system;
 
+
 CREATE OR REPLACE VIEW team_standing AS
-SELECT RANK() over (ORDER BY SUM(season_points) DESC, SUM(season_goals - season_goals_against) DESC) team_rank, t.name team_name, 
-       CONCAT(SUM(season_goals), ':', SUM(season_goals_against)) total_score, 
-       GROUP_CONCAT(CONCAT(s.name, ' (', season_points, ')') ORDER BY s.start_date DESC SEPARATOR ', ') AS season_points
-FROM (SELECT team_id, season_id, SUM(points) season_points, SUM(goals_for) season_goals, SUM(goals_against) season_goals_against
-      FROM standings st
-          JOIN match_days md on st.match_day_id = md.id
-      GROUP BY team_id, season_id) ts
-          JOIN seasons s on ts.season_id = s.id
-          RIGHT JOIN teams t on t.id = ts.team_id
+SELECT RANK() over (ORDER BY SUM(season_points) DESC, SUM(season_goals - season_goals_against) DESC) team_rank, t.name team_name,
+       CONCAT(SUM(season_goals), ':', SUM(season_goals_against)) total_score,
+       GROUP_CONCAT(CONCAT(s.name, ' (', season_points, ')') ORDER BY s.start_date DESC SEPARATOR ', ') AS points_by_season
+FROM teams t
+     LEFT JOIN (SELECT team_id, season_id, SUM(points) season_points, SUM(goals_for) season_goals, SUM(goals_against) season_goals_against
+                FROM standings s
+                         JOIN match_days md on s.match_day_id = md.id
+                GROUP BY team_id, season_id) ts on t.id = ts.team_id
+     LEFT JOIN seasons s on ts.season_id = s.id
 GROUP BY t.id
 ORDER BY team_rank;
 
--- teams standing across all matches 
+-- global team ranking
 SELECT *
 FROM team_standing;
 
@@ -24,5 +25,34 @@ LIMIT 5;
 
 -- teams with no matches yet
 SELECT team_name
-FROM team_standing
-WHERE season_points IS NULL;
+FROM team_standing ts
+WHERE ts.points_by_season IS NULL;
+
+
+
+CREATE OR REPLACE VIEW strikers_stats AS
+SELECT p.name player_name, total_goals, goals_by_team, goals_by_season
+FROM players p
+    LEFT JOIN (SELECT player_id, GROUP_CONCAT(CONCAT(t.name, ' (', team_goals, ')') ORDER BY team_goals SEPARATOR ', ') goals_by_team 
+               FROM (SELECT player_id, team_id, COUNT(g.id) team_goals
+                     FROM goals g
+                        JOIN attendees a on g.attendee_id = a.id
+                     GROUP BY player_id, team_id) tg
+                    JOIN teams t on tg.team_id = t.id
+               GROUP BY player_id) by_team on p.id = by_team.player_id
+    LEFT JOIN (SELECT player_id, SUM(season_goals) total_goals, GROUP_CONCAT(CONCAT(s.name, ' (', season_goals, ')') ORDER BY s.start_date DESC SEPARATOR ', ') goals_by_season 
+               FROM (SELECT player_id, season_id, COUNT(g.id) season_goals
+                     FROM goals g
+                        JOIN attendees a on g.attendee_id = a.id
+                        JOIN matches m on a.match_id = m.id
+                        JOIN match_days md on m.match_day_id = md.id
+                     GROUP BY player_id, season_id) sg
+                    JOIN seasons s on sg.season_id = s.id
+               GROUP BY player_id) by_season ON p.id = by_season.player_id
+ORDER BY total_goals DESC;
+
+-- strikers goals distribution
+SELECT *
+FROM strikers_stats;
+
+
