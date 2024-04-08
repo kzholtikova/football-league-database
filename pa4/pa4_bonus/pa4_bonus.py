12ba9a5544222641d56241a4f3235d4f616e2703
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-04-08 11:43:31 +0300

    refactoring
    - try-except structure

--- pa4/pa4_bonus/pa4_bonus.py
+++ pa4/pa4_bonus/pa4_bonus.py
@@ -1,14 +1,5 @@
 import mysql.connector
 
-
-def rollback():
-    db.rollback()
-    print("Transaction rolled back due to the inconsistent data.")
-
-    db.close()
-    exit()
-
-
 db = mysql.connector.connect(
     host="localhost",
     port=3306,
@@ -19,25 +10,34 @@ db = mysql.connector.connect(
 )
 c = db.cursor()
 
-# update standing goals_for
-db.start_transaction()
+try:
+    # update standing goals_for
+    db.start_transaction()
 
-match_day_id = input("Enter match day id: ")  # 1
-team_id = input("Enter team id: ")  # 6
-match_id = input("Enter match id: ")  # 7 (team 10) - valid, 19 (team 30) - invalid
-# team 6 - 2:2   team 10 - 4:2   team 30 - 3:2
+    match_day_id = input("Enter match day id: ")  # 1
+    team_id = input("Enter team id: ")  # 6
+    match_id = input("Enter match id: ")  # 7 (team 10) - valid, 19 (team 30) - invalid
+    # team 6 - 2:2   team 10 - 4:2   team 30 - 3:2
 
-standings_update = ("UPDATE standings s SET goals_for = goals_for + 1 "
-                    f"WHERE match_day_id = {match_day_id} and team_id = {team_id};")
-c.execute(standings_update)
-if c.rowcount != 1:
-    rollback()
+    standings_update = ("UPDATE standings s SET goals_for = goals_for + 1 "
+                        f"WHERE match_day_id = {match_day_id} and team_id = {team_id};")
+    c.execute(standings_update)
+    if c.rowcount != 1:
+        db.rollback()
+        print("Transaction rolled back due to the inconsistent match_day_id - team_id data.")
 
-standings_opponent_update = (f"UPDATE standings s JOIN football_system.attendees a on s.team_id = a.team_id "
-                             f"SET goals_for = goals_against + 1 WHERE match_day_id = {match_day_id} "
-                             f"and match_id = {match_id} and a.team_id != {team_id};")
-c.execute(standings_opponent_update)
-if c.rowcount != 1:
-    rollback()
+    standings_opponent_update = (f"UPDATE standings s JOIN football_system.attendees a on s.team_id = a.team_id "
+                                 f"SET goals_for = goals_against + 1 WHERE match_day_id = {match_day_id} "
+                                 f"and match_id = {match_id} and a.team_id != {team_id};")
+    c.execute(standings_opponent_update)
+    if c.rowcount != 1:
+        db.rollback()
+        print("Transaction rolled back due to the inconsistent match_id - team_id data.")
 
-db.commit() # team 6 - 3:2   team 10 - 4:3   team 30 - 3:2
+    db.commit()  # team 6 - 3:2   team 10 - 4:3   team 30 - 3:2
+    print("Transaction's successful.")
+except mysql.connector.Error as e:
+    db.rollback()
+    print("Transaction rolled back: ", e)
+finally:
+    db.close()
